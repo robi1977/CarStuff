8a09d711680cea010b5fd095514d33ebf6a377db
rafal_robi_nowak <rafal.robi.nowak@gmail.com>
2020-02-27 09:54:49 +0100

    first step of order processorder
    pierwsze uzupełnienie pliku w podstawowy html
    i linijka sprawdzająca działanie procesora php

---
+++ php/processorder.php
@@ -0,0 +1,16 @@
+<!DOCTYPE php>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./css/processorder.css" />
+    <title>Wyniki zamówienia</title>
+  </head>
+  <body>
+    <h1>Części samochodowe - wyniki zamówienia</h1>
+
+    <?php
+        echo '<p>Zamówienie przyjęte</p>';
+    ?>
+
+  </body>
+</html>
