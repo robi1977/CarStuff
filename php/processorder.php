76d48e9c22f08b52bc8f6a9f6891c45e83a4fe6e
rafal_robi_nowak <rafal.robi.nowak@gmail.com>
2020-02-27 11:30:22 +0100

    move data from POST to variable and representation in table

--- php/processorder.php
+++ php/processorder.php
@@ -1,3 +1,8 @@
+<?php
+    $iloscopon=$_POST["iloscopon"];
+    $iloscoleju=$_POST["iloscoleju"];
+    $iloscswiec=$_POST["iloscswiec"];
+?>
 <!DOCTYPE html>
 <html lang="pl">
   <head>
@@ -10,6 +15,13 @@
 
     <?php
         echo '<p>Zamówienie przyjęte o: </p>'. date("Y-m-d H:i");
+        echo '<hr/>';
+        echo '<table class="order"><thead><tr><th>Co?</th><th>Ilość:</th></tr></thead>';
+        echo '<tbody>';
+        echo '<tr><td>Opon:</td><td>'.$iloscopon.'</td></tr>';
+        echo '<tr><td>Olej:</td><td>'.$iloscoleju.'</td></tr>';
+        echo '<tr><td>Świec:</td><td>'.$iloscswiec.'</td></tr>';
+        echo '</tbody></table>';
     ?>
 
   </body>
