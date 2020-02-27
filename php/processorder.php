fc4f61491ca7d332b4253358f9631e165e870ab9
rafal_robi_nowak <rafal.robi.nowak@gmail.com>
2020-02-27 11:46:41 +0100

    add links to css files

--- php/processorder.php
+++ php/processorder.php
@@ -7,7 +7,8 @@
 <html lang="pl">
   <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="./css/processorder.css" />
+    <link rel="stylesheet" type="text/css" href="../css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../css/processorder.css" />
     <title>Wyniki zamówienia</title>
   </head>
   <body>
@@ -16,11 +17,11 @@
     <?php
         echo '<p>Zamówienie przyjęte o: </p>'. date("Y-m-d H:i");
         echo '<hr/>';
-        echo '<table class="order"><thead><tr><th>Co?</th><th>Ilość:</th></tr></thead>';
+        echo '<table class="order"><thead><tr><th>Produkt:</th><th>Ilość:</th></tr></thead>';
         echo '<tbody>';
-        echo '<tr><td>Opon:</td><td>'.$iloscopon.'</td></tr>';
+        echo '<tr><td>Opony:</td><td>'.$iloscopon.'</td></tr>';
         echo '<tr><td>Olej:</td><td>'.$iloscoleju.'</td></tr>';
-        echo '<tr><td>Świec:</td><td>'.$iloscswiec.'</td></tr>';
+        echo '<tr><td>Świece zapłonowe:</td><td>'.$iloscswiec.'</td></tr>';
         echo '</tbody></table>';
     ?>
 
